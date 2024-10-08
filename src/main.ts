1c2802cad89a0bd15f5dc7b91e460014f878bcfb
Ivan-Andrusyshyn <ivan.andrusyshyn@icloud.com>
2024-10-08 20:24:53 +0300

    add env port

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,17 @@ import helmet from 'helmet';
 
 import { HttpExceptionFilter } from './ExceptionFilter';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = +configService.get<number>('DB_PORT');
+
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
   app.use(helmet());
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
