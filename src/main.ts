d80939504c50d0fbdac4d9a144d9efd470fa36f7
Ivan-Andrusyshyn <ivan.andrusyshyn@icloud.com>
2024-10-08 21:43:50 +0300

    fo bug with PORT

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,10 @@ import helmet from 'helmet';
 
 import { HttpExceptionFilter } from './ExceptionFilter';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
-  const port = +configService.get<number>('DB_PORT');
+  const port = process.env.PORT || 3000;
 
   app.enableCors({
     origin: '*',
